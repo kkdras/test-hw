d4d375af60ed2f5397690e94a6f0b8a44880b340
Константин Козлов <kkdras@MacBook-Pro-Konstantin.local>
2023-07-06 11:57:41 +0300

    add test bug 2

--- test/hermione/example.hermione.js
+++ test/hermione/example.hermione.js
@@ -1,7 +1,11 @@
 const { assert } = require('chai');
 const { resolutions } = require('./const');
 const { hasClass } = require('./utils');
-const axios = require('axios')
+const axios = require('axios');
+
+const getShortProducts = () => axios.get(`http://localhost:3000/hw/store/api/products`);
+const getProductByID = (id) => axios.get(`http://localhost:3000/hw/store/api/products/${id}`);
+const checkout = (form, cart) => axios.post(`http://localhost:3000/hw/store/api/checkout`, { form, cart });
 
 describe('интеграционные тесты.', async function() {
    it('после перехода по разделю мобильного меню оно должно закрыться', async function () {
@@ -31,11 +35,6 @@ describe('интеграционные тесты.', async function() {
    it('кнопка должна быть правильного размера', async function () {
       await this.browser.url('http://localhost:3000/hw/store/catalog/0');
 
-      this.browser.setWindowSize(
-         resolutions.mobile.width,
-         resolutions.mobile.height
-      );
-
       await this.browser.assertView(
          'compare buttons',
          '.ProductDetails-AddToCart'
@@ -43,24 +42,35 @@ describe('интеграционные тесты.', async function() {
    });
 
    it('GET-запрос возвращает запись с тем же id', async function () {
-      const shortProducts = await axios.get(`http://localhost:3000/hw/store/api/products`);
+      const shortProducts = await getShortProducts();
 
       const someShortProduct = shortProducts.data[shortProducts.data.length - 1];
 
-      const id = someShortProduct.id;
-
-      const fullProduct = await axios.get(`http://localhost:3000/hw/store/api/products/${id}`);
+      const fullProduct = await getProductByID(someShortProduct.id);
 
       assert.equal(someShortProduct.id, fullProduct.data.id);
    });
 
    it('при GET-запросе продуктов у каждого продукта должны быть обязательные поля', async () => {
-      const shortProducts = await axios.get(`http://localhost:3000/hw/store/api/products`);
+      const shortProducts = await getShortProducts();
 
       for (let shortProduct of shortProducts.data) {
          assert.typeOf(shortProduct.name, 'string', 'Имя должно быть строкой');
          assert.typeOf(shortProduct.id, 'number', 'Id должен быть числом');
          assert.typeOf(shortProduct.price, 'number', 'Стоимость должна быть числом');
       }
+   });
+
+   it('id заказа должен увеличиваться в инкрементальном порядке', async function () {
+      const orderData1 = await checkout({}, {});
+
+      await this.browser.pause(50);
+
+      const orderData2 = await checkout({}, {});
+
+      const diff = Math.abs(orderData1.data.id - orderData2.data.id);
+      const expectedDiff = 1;
+
+      assert.equal(diff, expectedDiff, 'Id\'s должны отличаться на единицу');
    });
 });
